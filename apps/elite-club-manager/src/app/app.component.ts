b086856ce41099f7b49b5371aae4f0b82414b438
Oscar Nicolas Lagatta <oscarlagatta@gmail.com>
2024-01-27 17:37:56 +0000

    Revert "Update AppComponent in elite-club-manager project"
    This reverts commit 893e05049fd8123682db5622d11f08589dda2048.

--- apps/elite-club-manager/src/app/app.component.ts
+++ apps/elite-club-manager/src/app/app.component.ts
@@ -1,19 +1,14 @@
-
-
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-
-
-
   standalone: true,
   imports: [NxWelcomeComponent, RouterModule],
-                                selector: 'padel-hub-suite-root',
+  selector: 'padel-hub-suite-root',
   templateUrl: './app.component.html',
-                      styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-                                      title = 'elite-club-manager';
+  title = 'elite-club-manager';
 }
